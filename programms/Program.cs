3d075b56dc32f26814e5955fd0d3f87c2939685c
Maksim <norten1156@gmail.com>
2023-06-21 16:20:53 +0300

    решил вторую задачу

--- programms/Program.cs
+++ programms/Program.cs
@@ -1,4 +1,4 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// 1)Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 /*int numberA = ReadInt("Введите число A: ");
 int numberB = ReadInt("Введите число B: ");
 ToDegree(numberA, numberB);
@@ -19,5 +19,41 @@ void ToDegree(int a, int b)
 int ReadInt(string message)
 {
     Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}*/
+// 2)Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*int number = ReadInt("Введите число: ");
+
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"Сумма цифр = {sum}");
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }*/
