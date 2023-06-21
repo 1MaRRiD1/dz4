c83bc0b5f2b47b315c52e139cced5ab3a9f81f61
Maksim <norten1156@gmail.com>
2023-06-21 17:09:08 +0300

    решил крайнюю задачу и редактировал код

--- programms/Program.cs
+++ programms/Program.cs
@@ -1,25 +1,33 @@
 ﻿// 1)Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-/*int numberA = ReadInt("Введите число A: ");
-int numberB = ReadInt("Введите число B: ");
-ToDegree(numberA, numberB);
-
-
-// Функция возведения в степень
-void ToDegree(int a, int b)
+/*
+internal class Program
 {
-    int result = 1;
-    for (int i = 1; i <= b; i++)
+    private static void Main(string[] args)
     {
-        result = result * a;
-    }
-    Console.WriteLine(result);
-}
+        
+        int numberA = ReadInt("Введите число A: ");
+        int numberB = ReadInt("Введите число B: ");
+        ToDegree(numberA, numberB);
 
-// Функция ввода
-int ReadInt(string message)
-{
-    Console.WriteLine(message);
-    return Convert.ToInt32(Console.ReadLine());
+
+        // Функция возведения в степень
+        void ToDegree(int a, int b)
+        {
+            int result = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                result = result * a;
+            }
+            Console.WriteLine(result);
+        }
+
+        // Функция ввода
+        int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
 }*/
 // 2)Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 /*int number = ReadInt("Введите число: ");
@@ -57,3 +65,29 @@ int ReadInt(string message)
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }*/
+//3)Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 3) Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+        int lenArray = ReadInt("Введите длинну массива: ");
+
+        int[] randomArray = new int[lenArray];
+        for (int i = 0; i < randomArray.Length; i++)
+        {
+            randomArray[i] = new Random().Next(0, 100);
+            Console.Write($"[{randomArray[i]}]  " );
+        }
+
+
+        // Функция ввода
+        int ReadInt(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+*/
